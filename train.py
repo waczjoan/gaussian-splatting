cd69c0c9fe047ff9477d73a108b11dbc7aa4b2e6
piotr310100 <56043614+piotr310100@users.noreply.github.com>
2024-01-23 00:15:54 +0100

    Change save_xyz

--- train.py
+++ train.py
@@ -224,7 +224,7 @@ if __name__ == "__main__":
     parser.add_argument("--quiet", action="store_true")
     parser.add_argument("--checkpoint_iterations", nargs="+", type=int, default=[])
     parser.add_argument("--start_checkpoint", type=str, default = None)
-    parser.add_argument("--save_xyz", type=bool, default = True)
+    parser.add_argument("--save_xyz", action='store_true')
     args = parser.parse_args(sys.argv[1:])
     args.save_iterations.append(args.iterations)
     
